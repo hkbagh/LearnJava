0d6913e90884f1e4fd7d82fdc7f4110f6569249f
Harekrishna Bagh <harekrishnabagh14@gmail.com>
2024-08-16 00:25:21 +0530

    I Learned how to create another branch in git.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
        JOptionPane.showMessageDialog(null, "Hello World");
        hkbagh.Progpram1.program1(new String[]{});
        hkbagh.Program2.main(new String[]{});
+       hkbagh.Program2.main(new String[]{});
 
     }
 }
