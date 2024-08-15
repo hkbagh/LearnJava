5830fe58e2d0a4a0ddd4927495087075bc4ebbc0
Harekrishna Bagh <harekrishnabagh14@gmail.com>
2024-08-16 00:07:15 +0530

    i learned how to use Math class floor

--- HkbaghFiles/src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-public class Main {
-    public static void main(String[] args) {
-
-        double no1 = Double.parseDouble(JOptionPane.showInputDialog("Enter Number1 :"));
-        double no2 = Math.floor(no1);
-        JOptionPane.showMessageDialog(null, "age: "+ no2);
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
 
-        double no1 = Double.parseDouble(JOptionPane.showInputDialog("Enter Number1 :"));
-        double no2 = Math.floor(no1);
-        JOptionPane.showMessageDialog(null, "age: "+ no2);
-        }
+       JOptionPane.showMessageDialog(null, "Hello World");
     }
+}
