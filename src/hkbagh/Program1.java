a347156a10f4b09821e9c5eda65d6e603ab1d67a
Harekrishna Bagh <harekrishnabagh14@gmail.com>
2024-08-16 11:10:10 +0530

    Merge pull request #1 from hkbagh/test-branch2
    rename

--- src/hkbagh/Program1.java
+++ src/hkbagh/Program1.java
@@ -2,7 +2,7 @@ package hkbagh;
 
 import javax.swing.*;
 
-public class Progpram1 {
+public class Program1 {
     public static void program1(String[] args) {
 
         JOptionPane.showMessageDialog(null, "Hello World from program 1");
